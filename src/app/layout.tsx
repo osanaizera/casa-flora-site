d36d85df0771a436f76d9f34aeed19964dc12d69
osanaizera <filipeosanai@gmail.com>
2026-03-06 17:10:08 -0300

    feat: add Meta Pixel to global layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -256,6 +256,19 @@ function gtag(){dataLayer.push(arguments);}
 gtag('js', new Date());
 gtag('config', 'G-G1Q6ZLFQJ7');`}
         </Script>
+        {/* Meta Pixel Code */}
+        <Script id="meta-pixel" strategy="afterInteractive">
+          {`!function(f,b,e,v,n,t,s)
+{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+n.queue=[];t=b.createElement(e);t.async=!0;
+t.src=v;s=b.getElementsByTagName(e)[0];
+s.parentNode.insertBefore(t,s)}(window, document,'script',
+'https://connect.facebook.net/en_US/fbevents.js');
+fbq('init', '1878286653011856');
+fbq('track', 'PageView');`}
+        </Script>
         {/* Schema.org LocalBusiness JSON-LD for GMB association */}
         <script
           type="application/ld+json"
@@ -273,6 +286,16 @@ gtag('config', 'G-G1Q6ZLFQJ7');`}
         />
       </head>
       <body className={`${archivo.variable} ${inter.variable} antialiased`}>
+        <noscript>
+          {/* eslint-disable-next-line @next/next/no-img-element */}
+          <img
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            src="https://www.facebook.com/tr?id=1878286653011856&ev=PageView&noscript=1"
+            alt=""
+          />
+        </noscript>
         {children}
         <ConsentBanner />
         <Analytics />
